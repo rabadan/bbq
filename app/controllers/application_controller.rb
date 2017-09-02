77ec386ee13594b7cb6322560759e7d63bfe1f84
rabadan731 <rabadan731@yandex.ru>
2017-09-02 17:21:14 +0300

    users and events update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  helper_method :current_user_can_edit?
+
   protected
 
   def configure_permitted_parameters
@@ -10,5 +12,10 @@ class ApplicationController < ActionController::Base
       u.permit({ roles: [] }, :password, :password_confirmation, :current_password)
     }
   end
-  
+
+  def current_user_can_edit?(event)
+    user_signed_in? && event.user == current_user
+  end
+
+
 end
