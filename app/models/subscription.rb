afceea26f18171be374373591a9a3f7897ce1e2e
rabadan731 <rabadan731@yandex.ru>
2017-09-06 20:35:05 +0300

    fix subscription: mail of an existing user

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,11 +4,13 @@ class Subscription < ApplicationRecord
 
   validates :event, presence: true
 
-  validates :user_name, presence: true, unless: 'user.present?'
-  validates :user_email, presence: true, format: { with: /\A[a-z\d\-_.]+@[a-z\d\-_.]+\z/i }, unless: 'user.present?'
-
   validates :user, uniqueness: { scope: :event_id }, if: 'user.present?'
+
+  validates :user_name, presence: true, unless: 'user.present?'
+
+  validates :user_email, presence: true, format: { with: /\A[a-z\d\-_.]+@[a-z\d\-_.]+\z/i }, unless: 'user.present?'
   validates :user_email, uniqueness: { scope: :event_id }, unless: 'user.present?'
+  validate :email_of_user, unless: 'user.present?'
 
   def user_name
     if user.present?
@@ -23,6 +25,14 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def email_of_user
+    if User.find_by(email: user_email)
+      errors.add(:user_email, I18n::t('subscriptions.subscription.mail_existing_user'))
     end
   end
 end
