8ee8dddfc623d9f9c24884a5fec874046793b71e
rabadan731 <rabadan731@yandex.ru>
2017-09-13 06:02:34 -0700

    Pin code for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,8 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show]
   before_action :set_current_user_event, only: [:edit, :update, :destroy]
 
+  before_action :password_guard!, only: [:show]
+
   # GET /events
   def index
     @events = Event.all
@@ -60,6 +62,39 @@ class EventsController < ApplicationController
   end
 
   def event_params
-    params.require(:event).permit(:title, :address, :datetime, :description)
+    params.require(:event).permit(:title, :address, :datetime, :description, :pincode)
+  end
+
+  # Убедитесь, что в secrets.yml задано значение для  secret_key_base
+  #
+  # production:
+  #   secret_key_base: <%= ENV["SECRET_KEY_BASE"] %>
+  #
+  # И оно работает (например на Хероку задана нужная переменная окружения)
+  #
+  # Тогда куки в рельсах 4 и старше по умолчанию шифруются и хранить там пинкод
+  # для данной задачи достаточно безопасно.
+  #
+  # http://api.rubyonrails.org/classes/ActionDispatch/Session/CookieStore.html
+  #
+  def password_guard!
+    return true if @event.pincode.blank?
+    return true if signed_in? && current_user == @event.user
+
+    # Юзер на чужом событии (или не за логином). Проверяем, правильно ли передал
+    # пинкод. Если правильно, запоминаем в куках этого юзера этот пинкод для
+    # данного события.
+    if params[:pincode].present? && @event.pincode_valid?(params[:pincode])
+      cookies.permanent["events_#{@event.id}_pincode"] = params[:pincode]
+    end
+
+    # Проверяем — верный ли в куках пинкод, если нет — ругаемся и рендерим форму
+    pincode = cookies.permanent["events_#{@event.id}_pincode"]
+    unless @event.pincode_valid?(pincode)
+      if params[:pincode].present?
+        flash.now[:alert] = I18n.t('controllers.events.wrong_pincode')
+      end
+      render 'password_form'
+    end
   end
 end
