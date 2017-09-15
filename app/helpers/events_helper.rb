a40bcc2a7473910b74e01a069316839743a90976
rabadan731 <rabadan731@yandex.ru>
2017-09-15 11:38:28 +0300

    fix can_subscribe_event?

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,9 @@
 module EventsHelper
+  def can_subscribe_event?
+    if current_user.present?
+      @event.user != current_user && !@event.subscriptions.map(&:user).include?(current_user)
+    else
+      true
+    end
+  end
 end
